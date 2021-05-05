40e72e012077c80020a6bd7c17ed3258f4a70b78
Victor <victor_raony12@hotmail.com>
2021-05-05 09:04:20 -0400

    First commit

--- api/serializers.py
+++ api/serializers.py
@@ -4,9 +4,9 @@ from .models import *
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vendor
-        fields = ["id", "nome_vendor", "email", "status", "city"]
+        fields = ["id", "nome_vendor", "email", "status"]
 
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
-        fields = ["id", "city", "state"]
+        fields = ["id", "city", "state", "vendor_id"]
--- api/views.py
+++ api/views.py
@@ -26,18 +26,16 @@ class VendorList(APIView):
     def post(self, request):
         try:
             serializer = VendorSerializer(data=request.data)
-            city_id = request.data['city']
-            City.objects.get(pk=city_id)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except City.DoesNotExist:
-            return JsonResponse({'mensagem': "A cidade selecionada não esta cadastrada ou não existe."},
+        except Vendor.DoesNotExist:
+            return JsonResponse({'mensagem': "A empresa a ser relacionada não existe"},
                                 status=status.HTTP_404_NOT_FOUND)
         except Exception:
             return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
-                                 status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class VendorDetalhes(APIView):
     def get(self, request, pk):
@@ -62,12 +60,17 @@ class VendorDetalhes(APIView):
                                     status=status.HTTP_400_BAD_REQUEST)
             vendor = Vendor.objects.get(pk=pk)
             serializer = VendorSerializer(vendor, data=request.data)
+            city_id = request.data['city']
+            City.objects.get(pk=city_id)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Vendor.DoesNotExist:
             return JsonResponse({'mensagem': "O vendedor não existe"},
+                                status=status.HTTP_404_NOT_FOUND)
+        except City.DoesNotExist:
+            return JsonResponse({'mensagem': "A empresa a ser relacionada não existe"},
                                 status=status.HTTP_404_NOT_FOUND)
         except Exception:
             return  JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
@@ -92,8 +95,10 @@ class CityList(APIView):
     def get(self, request):
         try:
             lista_citys = City.objects.all()
-            serializer = CitySerializer(lista_citys, many=True)
-            return Response(serializer.data)
+            paginator = PaginacaoCity()
+            result_page = paginator.paginate_queryset(lista_citys, request)
+            serializer = CitySerializer(result_page, many=True)
+            return paginator.get_paginated_response(serializer.data)
         except Exception:
             return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
                                 status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -118,6 +123,39 @@ class CityDetalhes(APIView):
             city = City.objects.get(pk=pk)
             serializer = CitySerializer(city)
             return Response(serializer.data)
+        except City.DoesNotExist:
+            return JsonResponse({'mensagem': "A cidade não existe"},
+                                status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def put(self, request, pk):
+        try:
+            if pk == "0":
+                return JsonResponse({'mensagem': "O ID deve ser maior que zero."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+            city = City.objects.get(pk=pk)
+            serializer = CitySerializer(city, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except City.DoesNotExist:
+            return JsonResponse({'mensagem': "Cidade não cadastrada"},
+                                status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            return  JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
+                                 status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def delete(self, request, pk):
+        try:
+            if pk == "0":
+                return JsonResponse({'mensagem': "O ID deve ser maior que zero."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+            city = City.objects.get(pk=pk)
+            city.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
         except City.DoesNotExist:
             return JsonResponse({'mensagem': "A cidade não existe"},
                                 status=status.HTTP_404_NOT_FOUND)
